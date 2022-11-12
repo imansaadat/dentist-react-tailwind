37c8dc02fe806e0bf79e3f1c41df376acd315ada
Eslam Saadat (Iman) <80543205+eslamsaadat@users.noreply.github.com>
2022-11-12 09:09:58 +0330

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({text,btnStyle}) => {
   return (
     <>
-    <button className={`text-white bg-blue py-4 px-12 rounded hover:bg-black inline-block ${btnStyle}`}>{text}</button>
+    <button className={`text-white bg-blue py-4 px-12 rounded hover:bg-black inline-block hover:duration-300 ${btnStyle}`}>{text}</button>
     </>
   )
 }
