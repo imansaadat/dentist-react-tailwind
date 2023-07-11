a8ee894ed249635e2e791e8deb297c576138c4d1
Iman Saadat <e.saadat72@gmail.com>
2023-07-11 15:31:28 +0330

    Navbar was updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,58 +1,86 @@
-import React, { useState,useEffect } from 'react'
-import NavLinks from './NavLinks'
+import React, { useState, useEffect } from "react";
+import NavLinks from "./NavLinks";
 // react scroll
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 // fontawesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars , faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 // button component
-import Button from '../Button';
+import Button from "../Button";
+// // NavLink Data
+import { navLinksData } from "../../data/data";
+import ResponsiveMenu from "./ResponsiveMenu";
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
-  const [bg, setBg] = useState(false)
+  const [nav, setNav] = useState(false);
+  const [bg, setBg] = useState(false);
 
   const handleNav = () => {
-     setNav(!nav)
-  }
+    setNav(!nav);
+  };
 
-   useEffect(()=>{
-   const handleScroll = () =>{
-      if(window.scrollY){
-         setBg(true)
-      }else{
-         setBg(false)
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY) {
+        setBg(true);
+      } else {
+        setBg(false);
       }
-   }  
-   window.addEventListener('scroll',handleScroll)
-  },[])
-  
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <header className={`fixed w-full left-0 top-0 z-10 duration-300 ${bg ? 'bg-white shadow-md':''}`}>
-    <nav className='flex justify-between items-center max-w-[1200px] mx-auto px-5 h-20'>
-       <Link to="home" smooth={true} duration={500} className='z-10 cursor-pointer'>
-       <h1 className={`text-2xl lg:text-3xl ${nav ? 'text-white' : 'text-black'}`}
-            >Dental<span className="text-blue">Care.</span></ h1>
-       </Link>
-       <div className='hidden md:flex'>
-          <ul className='flex space-x-8'>
-             <NavLinks />
+    <header
+      className={`fixed w-full left-0 top-0 z-10 duration-300 ${
+        bg ? "bg-white shadow-md" : ""
+      }`}
+    >
+      <nav className="flex justify-between items-center max-w-[1200px] mx-auto px-5 h-20">
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          className="z-10 cursor-pointer"
+        >
+          <h1
+            className={`text-2xl lg:text-3xl ${
+              nav ? "text-white" : "text-black"
+            }`}
+          >
+            Dental<span className="text-blue">Care.</span>
+          </h1>
+        </Link>
+        <div className="hidden md:flex">
+          <ul className="flex space-x-8">
+            {navLinksData.map((item, index) => (
+              <NavLinks item={item} key={index} handleNav={handleNav}/>
+            ))}
           </ul>
-       </div>
-          <Button text='make appointment' btnStyle='hidden lg:flex' />
-       {/* Hamburger icon */}
-       <div className={`flex md:hidden cursor-pointer z-10 hover:text-blue ${nav ? 'text-white':'text-black'}`} onClick={handleNav}>
-          {nav ? <FontAwesomeIcon icon={faTimes} className='text-3xl'></FontAwesomeIcon> : <FontAwesomeIcon icon={faBars} className='text-3xl' ></FontAwesomeIcon>}
-       </div>
+        </div>
+        <Button text="make appointment" btnStyle="hidden lg:flex" />
+        {/* Hamburger icon */}
+        <div
+          className={`flex md:hidden cursor-pointer z-10 hover:text-blue ${
+            nav ? "text-white" : "text-black"
+          }`}
+          onClick={handleNav}
+        >
+          {nav ? (
+            <FontAwesomeIcon
+              icon={faTimes}
+              className="text-3xl"
+            ></FontAwesomeIcon>
+          ) : (
+            <FontAwesomeIcon
+              icon={faBars}
+              className="text-3xl"
+            ></FontAwesomeIcon>
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <ResponsiveMenu navLinksData={navLinksData} nav={nav} handleNav={handleNav}/>
+      </nav>
+    </header>
+  );
+};
 
-       {/* Mobile menu */}
-       <div className={`flex flex-col justify-center items-center md:hidden w-full absolute top-0 duration-500 h-screen bg-[rgba(0,0,0,.9)] ${nav ? 'left-0' : 'left-[-100%]'}`}>
-          <ul className='flex flex-col items-center space-y-6'>
-             <NavLinks handleNav={handleNav} />
-          </ul>
-       </div>
-    </nav>
- </header>
-  )
-}
-
-export default Navbar
+export default Navbar;
---
+++ src/data/data.js
@@ -0,0 +1,23 @@
+// NavLink Data
+export const navLinksData = [
+    {
+      name: 'Home',
+      path: 'home'
+    },
+    {
+      name: 'About',
+      path: 'about'
+    },
+    {
+      name: 'Services',
+      path: 'services'
+    },
+    {
+      name: 'Reviews',
+      path: 'reviews'
+    },
+    {
+      name: 'Contact',
+      path: 'contact'
+    }
+  ]
